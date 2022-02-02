a441a79b20ed3249db1d4222f118b7cf36a02fca
Tang Jiaxing <tangjiaxing1988@163.com>
2022-02-02 19:22:34 +0800

    click to start game

--- example/index.js
+++ example/index.js
@@ -6,6 +6,36 @@ fetch("game.nes", {
 })
 .then(res => res.arrayBuffer())
 .then(data => {
-    const emulator = wasm.create_emulator(document.getElementById("game"), {});
-    emulator.insert(new Uint8Array(data));
+    const wrapper = document.getElementById("game");
+    const emulator = wasm.create_emulator(wrapper, {});
+    
+    const button = createButton(wrapper);
+    button.onclick = function() {
+        button.style.display = "none";
+        emulator.insert(new Uint8Array(data));
+    }
 })
+
+// Web audio can only be played after a user event,
+// So we insert a button here and user must click it to start the game.
+function createButton(wrapper) {
+    const button = document.createElement("div");
+    button.style.cursor = "pointer";
+    button.style.backgroundImage = "url(./play.svg)";
+    button.style.backgroundPosition = "center center";
+    button.style.backgroundSize = "15vw 15vw";
+    button.style.backgroundRepeat = "no-repeat";
+    button.style.position = "absolute";
+    button.style.left = 0;
+    button.style.top = 0;
+    document.body.appendChild(button);
+
+    function resize() {
+        button.style.width = wrapper.offsetWidth + "px";
+        button.style.height = wrapper.offsetHeight + "px";
+    }
+    resize();
+    window.addEventListener("resize", resize);
+
+    return button;
+}
