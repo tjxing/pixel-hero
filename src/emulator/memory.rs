30377a6d8da864282c08d6e4704a87310465bcde
Tang Jiaxing <tangjiaxing1988@163.com>
2022-03-14 01:14:46 +0800

    Add memory unit tests

--- src/emulator/memory.rs
+++ src/emulator/memory.rs
@@ -35,3 +35,74 @@ impl Memory {
         self.values[(addr & 0x07FF) as usize] = v;
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    pub fn test_read_zero_page() {
+        let mut memory = Memory::new();
+        memory.values[0] = 0xF1;
+        memory.values[100] = 0xF2;
+        memory.values[255] = 0xF3;
+        assert_eq!(memory.read_zero_page(0), 0xF1);
+        assert_eq!(memory.read_zero_page(100), 0xF2);
+        assert_eq!(memory.read_zero_page(255), 0xF3);
+    }
+
+    #[test]
+    pub fn test_write_zero_page() {
+        let mut memory = Memory::new();
+        memory.write_zero_page(0, 0xF1);
+        memory.write_zero_page(100, 0xF2);
+        memory.write_zero_page(255, 0xF3);
+        assert_eq!(memory.values[0], 0xF1);
+        assert_eq!(memory.values[100], 0xF2);
+        assert_eq!(memory.values[255], 0xF3);
+    }
+
+    #[test]
+    pub fn test_read_stack() {
+        let mut memory = Memory::new();
+        memory.values[256] = 0xF1;
+        memory.values[256 + 100] = 0xF2;
+        memory.values[256 + 255] = 0xF3;
+        assert_eq!(memory.read_stack(0), 0xF1);
+        assert_eq!(memory.read_stack(100), 0xF2);
+        assert_eq!(memory.read_stack(255), 0xF3);
+    }
+
+    #[test]
+    pub fn test_write_stack() {
+        let mut memory = Memory::new();
+        memory.write_stack(0, 0xF1);
+        memory.write_stack(100, 0xF2);
+        memory.write_stack(255, 0xF3);
+        assert_eq!(memory.values[256], 0xF1);
+        assert_eq!(memory.values[256 + 100], 0xF2);
+        assert_eq!(memory.values[256 + 255], 0xF3);
+    }
+
+    #[test]
+    pub fn test_read() {
+        let mut memory = Memory::new();
+        memory.values[0] = 0xF1;
+        memory.values[1024] = 0xF2;
+        memory.values[MEMORY_SIZE - 1] = 0xF3;
+        assert_eq!(memory.read(0), 0xF1);
+        assert_eq!(memory.read(1024), 0xF2);
+        assert_eq!(memory.read(MEMORY_SIZE as u16 - 1), 0xF3);
+    }
+
+    #[test]
+    pub fn test_write() {
+        let mut memory = Memory::new();
+        memory.write(0, 0xF1);
+        memory.write(1024, 0xF2);
+        memory.write(MEMORY_SIZE as u16 - 1, 0xF3);
+        assert_eq!(memory.values[0], 0xF1);
+        assert_eq!(memory.values[1024], 0xF2);
+        assert_eq!(memory.values[MEMORY_SIZE - 1], 0xF3);
+    }
+}
